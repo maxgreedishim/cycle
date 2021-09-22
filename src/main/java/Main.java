77da500ace3ddc28a020b9c3668ee00293e9c357
Денис <89923002057@mail.ru>
2021-09-22 22:21:00 +0500

    switch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        int age = 6;
+        switch (age){
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Вы младенец");
+                break;
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                System.out.println("Вы ребенок");
+                break;
+            default:
+                System.out.println("Вы взрослый");
+        }
 
     }
 }
